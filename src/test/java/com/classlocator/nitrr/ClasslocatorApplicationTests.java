40283cebdadf6658aaa197071820d3b8f68e51a4
Anuj Singh <anuj.as828@gmail.com>
2024-11-09 13:03:51 +0530

    initilized the project

---
+++ src/test/java/com/classlocator/nitrr/ClasslocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classlocator.nitrr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClasslocatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
